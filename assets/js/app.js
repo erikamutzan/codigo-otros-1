d83c69eebabd900aad5818286b55447cd5f2a62e
Erika Mutzan <erikamtzmutzan@gmail.com>
2025-02-10 00:26:03 -0600

    primer analisis y comentarios correccion

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,22 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name'); //se coloca un punto para identificar un estilo, va a guardar el elemento HTML con la clase especificada 
+const $b = document.querySelector('.blog'); //se retira # y se coloca el punto porque es un estilo
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser (username) { //se debe colocar async porque se sta utilizando await 
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  const data = await fetch(`${usersEndpoint}/${username}`); //se cambio la variable por data
+  console.log(data); //aqui ya se puede imprimir el resultado de la busqueda
+  $n.textContent = `${data.name}`; // se deben cambiar las comillas simples por las back stick
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
-  console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  console.log(err); // se imprime en consola
+  $n.textContent = `Algo salió mal: ${err}` //se agrego $ a n //se imprime en html
 }
 
 displayUser('stolinski').catch(handleError);
